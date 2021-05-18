dc9b3f6d56d945820e8ccee17131975c511193eb
sandil <sandilazad@gmail.com>
2021-05-18 13:16:25 +0600

    dawer

--- lib/Views/DashBoard/LandingPage/Maindrawer.dart
+++ lib/Views/DashBoard/LandingPage/Maindrawer.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:multilevel_drawer/multilevel_drawer.dart';
 import 'package:parts_peon/Model/Metarial/ColorConfig.dart';
 
 class Maindrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-        child: Column(
-      children: [
-        Container(
+    return SafeArea(
+      child: MultiLevelDrawer(
+        header: Container(
           width: double.infinity,
-          height: 200,
+          height: 220,
           color: ColorConfig.themeColour,
           padding: EdgeInsets.all(20),
           child: Column(
@@ -25,12 +25,103 @@ class Maindrawer extends StatelessWidget {
               ),
               Text(
                 'MD.Mahamudul Hasan',
-                style: TextStyle(fontSize: 20, color: Colors.white),
+                style: TextStyle(fontSize: 16, color: Colors.white),
               )
             ],
           ),
-        )
-      ],
-    ));
+        ),
+        children: [
+          MLMenuItem(
+              content: Text('Person'),
+              leading: Icon(Icons.person),
+              trailing: Icon(Icons.arrow_right),
+              onClick: () {},
+              subMenuItems: [
+                MLSubmenu(
+                    submenuContent: Text('Option 1'),
+                    onClick: () {
+                      Navigator.pop(context);
+                    })
+              ]),
+          MLMenuItem(
+              content: Text('Notification'),
+              leading: Icon(Icons.notifications),
+              trailing: Icon(Icons.arrow_right),
+              onClick: () {},
+              subMenuItems: [
+                MLSubmenu(submenuContent: Text('Option 1'), onClick: () {}),
+                MLSubmenu(submenuContent: Text('Option 2'), onClick: () {}),
+                MLSubmenu(submenuContent: Text('Option 3'), onClick: () {}),
+              ]),
+          MLMenuItem(
+            content: Text('Exit'),
+            leading: Icon(Icons.exit_to_app),
+            onClick: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
+
+// Size size = MediaQuery.of(context).size;
+
+//     return MultiLevelDrawer(
+//       rippleColor: Colors.white,
+//       subMenuBackgroundColor: Colors.grey.shade100,
+//       divisionColor: Colors.grey,
+//       header: Container(
+//         color: ColorConfig.themeColour,
+//         height: size.height * 0.25,
+//         child: Center(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: <Widget>[
+//               // Image.asset(
+//               //   // "assets/dp_default.png",
+//               //   width: 100,
+//               //   height: 100,
+//               // ),
+
+//               Icon(
+//                 Icons.account_circle,
+//                 size: 120,
+//                 color: Colors.white54,
+//               ),
+
+//               SizedBox(
+//                 height: 10,
+//               ),
+//               Text("MD.Mahamudul Hasan")
+//             ],
+//           ),
+//         ),
+//       ),
+//       children: [
+//         MLMenuItem(
+//             leading: Icon(Icons.settings),
+//             trailing: Icon(Icons.arrow_right),
+//             content: Text("Car"),
+//             onClick: () {},
+//             subMenuItems: [
+//               MLSubmenu(onClick: () {}, submenuContent: Text("Option 1")),
+//               MLSubmenu(onClick: () {}, submenuContent: Text("Option 2"))
+//             ]),
+//         MLMenuItem(
+//             leading: Icon(Icons.payment),
+//             trailing: Icon(Icons.arrow_right),
+//             content: Text(
+//               "Bike",
+//             ),
+//             subMenuItems: [
+//               MLSubmenu(onClick: () {}, submenuContent: Text("Option 1")),
+//               MLSubmenu(onClick: () {}, submenuContent: Text("Option 2")),
+//               MLSubmenu(onClick: () {}, submenuContent: Text("Option 3")),
+//               MLSubmenu(onClick: () {}, submenuContent: Text("Option 4")),
+//             ],
+//             onClick: () {}),
+//       ],
+//     );
+//   }
